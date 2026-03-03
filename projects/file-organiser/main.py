aae7f6f49edeef5f8af93453e18c9ba745e0793e
Harrison <166224036+H8991@users.noreply.github.com>
2026-03-03 14:34:21 +0000

    Full project + junkfolder

--- projects/file-organiser/main.py
+++ projects/file-organiser/main.py
@@ -2,7 +2,9 @@ import os
 import shutil
 
 #Welcome
-print("==== Hello, welcome to File Organiser. ====")
+print("=====================================")
+print("= Hello, welcome to File Organiser. =")
+print("=====================================")
 
 #Get path of junk folder
 junk_folder = input("Please paste the path of the folder you would like to organise:").strip().replace('"', '')
@@ -15,4 +17,41 @@ while not os.path.isdir(junk_folder):
 #Confirm folder has been found
 print("Thank you. Folder found.")
 
+#List files found in folder
+files = os.listdir(junk_folder)
+print("These are the files in that folder:\n")
+for file in files:
+    print(file)
+
+#Define sorting categories
+sorting_categories = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Documents": [".docx", ".doc", ".pdf", ".txt"],
+    "Audio": [".mp3", ".mp4", ".wav"]
+}
+
+#Sort files into categories
+#split name from extension
+for file in files:
+    name, extension = os.path.splitext(file)
+    extension = extension.lower()
+
+    #check each category to see if extension is suitable
+    for filetype, ext_list in sorting_categories.items():
+        if extension in ext_list:
+            target_folder = os.path.join(junk_folder, filetype)
+
+            #Create folder if not created yet
+            if not os.path.exists(target_folder):
+                os.makedirs(target_folder)
+
+            #Move the file
+            source = os.path.join(junk_folder, file)
+            destination = os.path.join(target_folder, file)
+            #Use shutil to move
+            shutil.move(source, destination)
+            print(f"Moved {file} to {target_folder} folder.")
+
+#Completion message
+print("All files in this folder have been organised.")
 
